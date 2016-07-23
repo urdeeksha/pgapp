dfb85462d2db0b9f1868235a8c9c9b98641afac6
Mohan R. Arai <mohanreddy.arai@gmail.com>
2016-07-23 21:03:44 +0530

    Java HelloWorld Program

---
+++ src/HelloWorld/HelloWorldMain.java
@@ -0,0 +1,21 @@
+/**
+ * Test Program to verify java and project is setup properly
+ * Output shall be printed to Console
+ */
+package HelloWorld;
+
+/**
+ * @author Mohan
+ *
+ */
+public class HelloWorldMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
